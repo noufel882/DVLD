16689fb57fac1c7376aa479de1f498c984313c01
noufel <noufel_882@outlook.com>
2026-03-13 18:20:46 +0100

    Add a simple class 'Hashing' with a simple method apply SHA-256

---
+++ Source Code/Uitils/Cryptography/Hashing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Utils.Cryptography
+{   
+    public class Hashing
+    {
+        public static string ComputeHash_SHA256(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+               
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
